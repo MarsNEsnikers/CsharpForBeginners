3cffd816bb843c587da55152fc279684ddea527a
MarsNEsnikers <79804851+MarsNEsnikers@users.noreply.github.com>
2021-04-06 01:49:28 +0500

    Добавил в репозиторий

---
+++ BlackJack/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BlackJack
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPlayerContinue = true;
+            bool isComputerContine = true;
+
+            Random random = new Random();
+
+            do
+            {
+                Console.WriteLine("Будешь тянуть курту? (y/n)");
+                string answer = Console.ReadLine();
+
+                if (answer == "y")
+                {
+                    Console.WriteLine("Вы тяните карту");
+                }
+
+                if (answer == "n")
+                {
+                    Console.WriteLine("Вы пассуете");
+                    isPlayerContinue = false;
+                }
+
+                //isComputerContine
+                int decisionNumber = 0;
+
+                decisionNumber = random.Next(0,2);
+
+                if (decisionNumber == 0)
+                {
+                    Console.WriteLine("Бот тяните карту");
+                }
+
+                if (decisionNumber == 1)
+                {
+                    Console.WriteLine("Бот пассуете");
+                    isComputerContine = false;
+                }
+            } while (isPlayerContinue || isComputerContine);
+
+            
+        }
+    }
+}
---
+++ notebook/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace notebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string[] names = new string[10];
+
+            names[0] = "Ivan";
+            names[1] = "Tolik";
+            names[2] = "Alex";
+            names[3] = "Mars";
+            names[4] = "Mihail";
+            names[5] = "Miha";
+
+            Console.WriteLine("Введите ваше имя:");
+            names[6] = Console.ReadLine();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+
+                Console.WriteLine(names[i]);
+
+            }
+            Console.WriteLine("Вваше имя:" + names[6]);
+
+
+            byte[] age = new byte[10];
+
+            age[0] = 18;
+            age[1] = 19;
+            age[2] = 18;
+            age[3] = 19;
+            age[4] = 28;
+            age[5] = 38;
+
+            string strAge;
+            byte byteValue = age[7];
+
+            Console.WriteLine("Введите ваш возраст:");
+            strAge = Console.ReadLine();
+
+            bool success = Byte.TryParse(strAge, out byteValue);
+
+            for (int i = 0; i < age.Length; i++)
+            {
+                Console.WriteLine(age[i]);
+            }
+            
+            if (success)
+            {
+
+                Console.WriteLine("Ваш возраст:" + byteValue);
+                byteValue = age[7];
+            }
+
+            if (!success)
+            {
+                Console.WriteLine("Вы ввели не число!");
+                strAge = Console.ReadLine();
+                Console.WriteLine("Ваш возраст:" + byteValue);
+                byteValue = age[7];
+
+            }
+
+            string[] phoneNum = new string[10];
+
+            phoneNum[0] = "8 999 999 99 79";
+            phoneNum[1] = "8 999 959 92 99";
+            phoneNum[2] = "8 999 949 99 29";
+            phoneNum[3] = "8 999 939 99 89";
+            phoneNum[4] = "8 999 929 91 91";
+            phoneNum[5] = "8 999 929 91 91";
+
+
+            Console.WriteLine("Введите номер телефона:");
+            phoneNum[6] = Console.ReadLine();
+
+            for (int i = 0; i < phoneNum.Length; i++)
+            {
+                Console.WriteLine(phoneNum[i]);
+
+            }
+
+            Console.WriteLine("Вваше имя:" + names[6]);
+            Console.WriteLine("Ваш возраст:" + strAge, byteValue);
+            Console.WriteLine("Ваш номер телефона:" + phoneNum[6]);
+
+
+
+
+
+
+
+
+        }
+    }
+}
